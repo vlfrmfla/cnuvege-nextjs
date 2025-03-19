88ee0216b796bd9922f4dd81923ace2c86e7cf06
vlfrmfla <kimdongpil2@gmail.com>
2025-03-19 14:24:20 +0900

    change class and research topics

--- app/classes/page.tsx
+++ app/classes/page.tsx
@@ -10,19 +10,68 @@ export default function Page() {
       <h1 className="text-4xl font-bold text-gray-900 mb-6">Classes</h1>
 
       {/* ✅ 학부 수업 */}
-      <h2 className="text-2xl font-semibold text-gray-800 border-b pb-2 mb-6">학부 수업</h2>
-      <ul className="list-disc pl-6 space-y-2 text-lg text-gray-700">
-        <li>채소원예학 및 실습</li>
-      </ul>
+      <table className="min-w-full table-auto border-collapse table-fixed">
+        <thead>
+          <tr>
+            <th className="border-b px-4 py-2 text-left w-1/4">Category</th>
+            <th className="border-b-2 px-4 py-2 text-left w-1/4">Class</th>
+            <th className="border-b-2 px-4 py-2 text-left w-1/4">Description</th>
+            <th className="border-b-2 px-4 py-2 text-left w-1/4">Credit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/* 세로로 3개 셀 병합 */}
+            <td className="border-b px-4 py-2 font-semibold text-gray-800" rowSpan={3}>
+              Alumni
+            </td>
+            <td className="border-b px-4 py-2">채소원예학 및 실습</td>
+            <td className="border-b px-4 py-2">원예작물 중 채소의 중요성 및 전반적 내용에 대해 이해하고, 재배 실습을 수행</td>
+            <td className="border-b px-4 py-2">전공필수(3-3-0)</td>
+          </tr>
+          <tr>
+            <td className="border-b px-4 py-2">토양과 비료의 이해</td>
+            <td className="border-b px-4 py-2">토양학 기초 및 시설재배 및 노지재배지의 관비 관리 방법</td>
+            <td className="border-b px-4 py-2">전공필수(3-3-0)</td>
+          </tr>
+          <tr>
+            <td className="border-b px-4 py-2">원예작물영양학</td>
+            <td className="border-b px-4 py-2">식물 영양학 지식의 형성과 영양생리학에 대한 강의 </td>
+            <td className="border-b px-4 py-2">전공심화(3-3-0)</td>
+          </tr>
 
-      {/* ✅ 대학원 수업 */}
-      <h2 className="not-prose text-2xl font-semibold text-gray-800 border-b pb-2">대학원 수업</h2>
-      <ul className="list-disc pl-6 space-y-2 text-lg text-gray-700">
-        <li>채소학 최신과제</li>
-        <li>채소원예학 특론</li>
-        <li>원예작물영양학 특론</li>
-        <li>양액재배학 특론</li>
-      </ul>
+        </tbody>
+      </table>
+
+      <table className="min-w-full table-auto border-collapse table-fixed">
+
+        <tbody>
+          <tr>
+            {/* 세로로 4개 셀 병합 */}
+            <td className="border-b px-4 py-2 font-semibold text-gray-800 text-left w-1/4" rowSpan={4}>
+              Graduated
+            </td>
+            <td className="border-b px-4 py-2 w-1/4">채소학 최신과제</td>
+            <td className="border-b px-4 py-2 w-1/4">최근 5년간의 채소학 연구 동향과 과제</td>
+            <td className="border-b px-4 py-2 w-1/4">대학원 수업(3-3-0)</td>
+          </tr>
+          <tr>
+            <td className="border-b px-4 py-2">채소원예학 특론</td>
+            <td className="border-b px-4 py-2">채소 재배 방법과 동향에 대한 이론적, 실용적 특론 강의</td>
+            <td className="border-b px-4 py-2">대학원 수업(3-3-0)</td>
+          </tr>
+          <tr>
+            <td className="border-b px-4 py-2">원예작물영양학 특론</td>
+            <td className="border-b px-4 py-2">원예작물의 영양에 관한 심화 이론</td>
+            <td className="border-b px-4 py-2">대학원 수업(3-3-0)</td>
+          </tr>
+          <tr>
+            <td className="border-b px-4 py-2">양액재배학 특론</td>
+            <td className="border-b px-4 py-2">수경재배 시스템 적용과 지하부 양수분 관리, 미생물 등 최신 연구 동향 분석</td>
+            <td className="border-b px-4 py-2">대학원 수업(3-3-0)</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   );
 }
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -8,6 +8,8 @@ const notoSansKR = Noto_Sans_KR({
 });
 
 
+
+
 export const metadata = {
   title: 'news',
   description: 'Read Lab news.',
@@ -16,6 +18,17 @@ export const metadata = {
 export default function Page() {
   return (
     <section className={`prose ${notoSansKR.className}`}>
+      <h1>
+      연구분야
+      </h1>
+      {/* 연구분야 항목들 */}
+      <ul className="list-disc pl-6 mb-6 text-gray-800" >
+        <li>원예작물 안정생산을 위한 식물 생리 반응 최적화 연구</li>
+        <li>수경재배 지상부 및 지하부 관리 최적화 기술</li>
+        <li>시설원예 채소 생산 전과정평가 방법론 적용</li>
+        <li>LED 활용 식물 광 생리 반응 연구</li>
+      </ul>
+
       <h1 className="">Publications (from 2024)</h1>
       <BlogPosts />
     </section>
